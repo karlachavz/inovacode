f3737dde4cb3f4f79895731c0df291a04706fb86
karlachavz <karlacelestechavezcastillo@gmail.com>
2025-11-19 14:11:55 -0600

    Add files via upload
    Se agrego primary key y unique key a las tablas

--- complementarias.sql
+++ complementarias.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 13-11-2025 a las 00:29:41
+-- Tiempo de generación: 19-11-2025 a las 21:08:47
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -41,7 +41,12 @@ CREATE TABLE `administrador` (
 INSERT INTO `administrador` (`id_admin`, `usuario`, `correo`, `contrasena`) VALUES
 (6, 'Donovan', 'donovan@gmail.com', '12345'),
 (7, 'Jhonatan', 'jhon@gmail.com', '12345'),
-(8, 'Sebastian', 'sebastian@gmail.com', '12345');
+(8, 'Sebastian', 'sebastian@gmail.com', '12345'),
+(9, 'Jhonatanhjkhj', 'jhon@gmail.com', '123456'),
+(10, 'Robertoooooo', 'DANIA@GMIAL.COM', '12345678'),
+(16, 'dfgg', 'holla@gmail.com', 'jksdjkfuif'),
+(19, 'Roberto', 'holla@gmail.com', 'dfjkkgvv'),
+(20, 'rejlw', 'roberto@gmail.com', 'rjweklrjwkljrlwjrkl');
 
 -- --------------------------------------------------------
 
@@ -66,8 +71,11 @@ CREATE TABLE `alumno` (
 INSERT INTO `alumno` (`control`, `nombre`, `apellido_paterno`, `apellido_materno`, `carrera`, `correo`, `contrasena`) VALUES
 (29, 'Juana', 'Ortiz', 'Sanchez', 'TICS', 'holla@gmail.com', '12345'),
 (290, 'fernandaaaa', 'Luna', 'Dominguez', 'TICS', 'fer@gmail.com', '12345'),
-(223107500, 'Amanda', 'Luna', 'Dominguez', 'Contaduria', 'jhon@gmail.com', '12345'),
-(223107403, 'Karla', 'Alcon', 'Dominguez', 'TICS', 'DANIA@GMIAL.COM', '12345678');
+(223107300, 'Monserrat', 'Castañeda', 'Amador', 'Ingeniería en Administración', 'DANIA@GMIAL.COM', 'BNMBNMBM'),
+(223107402, 'Karla', 'Alcon', 'Dominguez', 'ISC', 'holla@gmail.com', '12345678'),
+(223107403, 'Karla', 'Alcon', 'Dominguez', 'TICS', 'DANIA@GMIAL.COM', '12345678'),
+(223107450, 'Yoa', 'Alcon', 'Ochoa', 'TICS', 'holla@gmail.com', '123456'),
+(223107500, 'Amanda', 'Luna', 'Dominguez', 'Contaduria', 'jhon@gmail.com', '12345');
 
 --
 -- Índices para tablas volcadas
@@ -77,7 +85,14 @@ INSERT INTO `alumno` (`control`, `nombre`, `apellido_paterno`, `apellido_materno
 -- Indices de la tabla `administrador`
 --
 ALTER TABLE `administrador`
-  ADD PRIMARY KEY (`id_admin`);
+  ADD PRIMARY KEY (`id_admin`),
+  ADD UNIQUE KEY `usuario` (`usuario`);
+
+--
+-- Indices de la tabla `alumno`
+--
+ALTER TABLE `alumno`
+  ADD PRIMARY KEY (`control`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -87,7 +102,7 @@ ALTER TABLE `administrador`
 -- AUTO_INCREMENT de la tabla `administrador`
 --
 ALTER TABLE `administrador`
-  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
